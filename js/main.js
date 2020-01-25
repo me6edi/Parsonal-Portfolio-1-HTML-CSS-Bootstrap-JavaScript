16291bc54b61c485948f7e725c4c5aca18912249
Mehedi Amin <exited17@gmail.com>
2020-01-25 10:24:40 +0600

    Footer

--- js/main.js
+++ js/main.js
@@ -43,6 +43,20 @@ $('.about-area .owl-carousel').owlCarousel({
 
 })
 
+// sticky navigation menu
+let nav_offset_top = $('.header_area').height() + 50;
+function navbarFixed(){
+	if($('.header_area').length){
+		$(window).scroll(function(){
+			let scroll = $(window).scroll();
+			if(scroll >= nav_offset_top){
+				$('.header_area .main-menu').addClass('navbar_fixed');
+			}else{
+				$('.header_area .main-menu').removeClass('navbar_fixed'); 
+			}
+		})
+	}
+}
 
 });
 
